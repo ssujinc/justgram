52127ee91dfc724d35543806a76b9508b2e9d0bb
Sujin Choi <hissujinc@gmail.com>
2022-05-20 16:53:09 +0900

    Add: main.html(layout)

--- js/login.js
+++ js/login.js
@@ -6,14 +6,14 @@ let valId = false;
 let valPw = false;
 
 email.addEventListener("keyup", (e) => {
-    if ( email.value.includes("@") ){
+    if ( e.currentTarget.value.includes("@") ){
         valId = getValueValidation(e.currentTarget.value);
     } else {
         valId = false;
     }
 });
 pw.addEventListener("keyup", (e) => {
-    if( pw.value.length > 4 ){
+    if( e.currentTarget.value.length > 4 ){
         valPw = getValueValidation(e.currentTarget.value);
     } else {
         valPw = false;
@@ -30,17 +30,6 @@ const getValueValidation = (value) => {
 document.addEventListener("keyup", () => {
     valId && valPw ? loginBtn.disabled = false : loginBtn.disabled = true;
 });
-
-
-/* document.addEventListener("keyup", () => {
-    let valId = document.getElementById('email').value;
-    let valPw = document.getElementById('pw').value;
-    if (valPw != "" && valId != "") {
-        loginBtn.disabled = false;
-    } else {
-        loginBtn.disabled = true;
-    }
-}); */
 
 
 // const loginInput = () => {}
@@ -61,20 +50,3 @@ document.addEventListener("keyup", () => {
 //         valPw = false;
 //     }
 // });
-
-
-// email.addEventListener('keyup', function () {
-//     valId = getValueValidation(this.value);
-// });
-// pw.addEventListener('keyup', function () {
-//     valPw = getValueValidation(this.value);
-// })
-// function getValueValidation(value) {
-//    var valid = false;
-//    if (value != null && value != "") {
-//       valid = true;   
-//    }
-//    return valid;
-// }
-
-
